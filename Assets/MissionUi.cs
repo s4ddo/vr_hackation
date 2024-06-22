41bb22c2e260041318cefc2e19fec41288d8b2ef
s4ddo <ahmadramadan407@gmail.com>
2024-06-22 19:56:53 +0200

    aryan for fix

--- Assets/MissionUi.cs
+++ Assets/MissionUi.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class MissionUi : MonoBehaviour
 {
+
+    public GameObject ui;
+    public Material skybox;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ui.SetActive(true);
+        RenderSettings.skybox = skybox;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ui.SetActive(false);
+
     }
 
     // Update is called once per frame
